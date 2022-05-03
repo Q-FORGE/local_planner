3b8017311922e016e42bd50871edeef5ccb54582
Jackson <15jgme@queensu.ca>
2022-05-03 06:13:17 +0200

    cost param update

--- include/local_planner/cost_parameters.h
+++ include/local_planner/cost_parameters.h
@@ -6,6 +6,6 @@ struct costParameters {
   float yaw_cost_param = 8.0f;
   float pitch_cost_param = 100.f;
   float velocity_cost_param = 800.f; // was 8000 JE
-  float obstacle_cost_param = 0.5f; // was 0.5
+  float obstacle_cost_param = 1.0f; // was 0.5
 };
 }
